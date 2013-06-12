2cc89df8b1da917bd999d6b000e28b3985953e81
johnotander <johnotander@gmail.com>
2013-06-12 14:56:08 -0600

    Add typing/parsing of ids.

--- lib/cellularity.rb
+++ lib/cellularity.rb
@@ -5,5 +5,18 @@ require 'cellularity/imei'
 require 'cellularity/iccid'
 
 module Cellularity
-  
+  def self.determine_id_type(id)
+    return :esn   if Cellularity::Esn.new(id).valid?
+    return :imei  if Cellularity::Imei.new(id).valid?
+    return :iccid if Cellularity::Iccid.new(id).valid?
+  end
+
+  def self.parse_id(id)
+    case determine_id_type(id)
+    when :esn   then Cellularity::Esn.new(id)
+    when :imei  then Cellularity::Imei.new(id)
+    when :iccid then Cellularity::Iccid.new(id)
+    else nil
+    end
+  end
 end
